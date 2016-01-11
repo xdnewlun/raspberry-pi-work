6fcbf9b588445871b1b4d6a64dee8b6aca6059b6
xdnewlun <xdnewlun@yahoo.com>
2016-01-11 17:32:33 -0600

    added  Enemy System

--- project-1/enemy.py
+++ project-1/enemy.py
@@ -2,5 +2,31 @@ import RPi.GPIO as gpio
 import time
 import random
 
-def chooseEnemy():
-	print("Test")
+class goblinClass:
+	hp = 3,
+	attack = 1,
+	defence = 1,
+	name = "Goblin"
+
+class zombieClass:
+	hp = 3,
+	attack = 2,
+	defence = 0,
+	name = "Zombie"
+
+class skeletonClass:
+	hp = 6,
+	attack = 1,
+	defence = 2,
+	name = "Skeleton"
+
+goblin = goblinClass()
+zombie = zombieClass()
+skeleton = skeletonClass()
+
+enemies = [goblin, zombie, skeleton]
+
+def chooseEnemy(){
+	randomEnemy = rand.randint(0, len(enemies))
+	print(randomEnemy)
+}
