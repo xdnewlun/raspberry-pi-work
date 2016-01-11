265774930c328835b2caac0b18cc9898d9a473cb
xdnewlun <xdnewlun@yahoo.com>
2016-01-10 21:05:38 -0600

    edited stuffie stuffs

--- project-1/enemy.py
+++ project-1/enemy.py
@@ -1,25 +1,6 @@
 import RPi.GPIO as gpio
 import time
 import random
-import start
-import player
-import battle
-
-gpio.setmode(gpio.BCM)
-gpio.setwarnings(False)
-
-gpio.setup(17,gpio.IN) #Right Button
-gpio.setup(27,gpio.IN) #Left Button
-
-gpio.setup(13,gpio.OUT) #User RED LED
-gpio.setup(6, gpio.OUT) #User YELLOW LED
-gpio.setup(5,gpio.OUT) #User Green LED
-
-gpio.setup(23,gpio.OUT) #Enemy RED LED
-gpio.setup(24, gpio.OUT) #Enemy YELLOW LED
-gpio.setup(25,gpio.OUT) #Enemy Green LED
 
 def chooseEnemy():
 	print("Test")
-
-chooseEnemy()
