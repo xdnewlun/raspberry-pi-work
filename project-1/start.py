6f02ebd072c205cc4dbe4d2075a72d72b5d4047f
xdnewlun <xdnewlun@yahoo.com>
2016-01-10 21:29:03 -0600

    changed button

--- project-1/start.py
+++ project-1/start.py
@@ -25,7 +25,7 @@ for i in range(0, 100):
 def start():
     print("Please press the 'Action Button' To Start!")
     while True:
-    	if gpio.input(27):
+    	if gpio.input(17):
     		print("Not pressed")
     	else:
     		print("Pressed")
