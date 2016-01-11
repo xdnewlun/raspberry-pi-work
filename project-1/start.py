4ff9f2c8d949cc27a5358dacf7e0219699f72ac5
xdnewlun <xdnewlun@yahoo.com>
2016-01-10 21:13:12 -0600

    testing

--- project-1/start.py
+++ project-1/start.py
@@ -26,5 +26,7 @@ def start():
     print("Please press the 'Action Button' To Start!")
     #while True:
     if gpio.input(27) == False:
-        chooseEnemy()
+        print("?")
+    elif gpio.input(27) == True:
+    	print("!")
 start()
